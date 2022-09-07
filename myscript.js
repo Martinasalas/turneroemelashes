6c09dbda52d35ebbc034d0f86749c7ed2f74963d
Martina Salas <martinasalas08@gmail.com>
2022-09-07 19:32:37 -0400

    pf1

--- myscript.js
+++ myscript.js
@@ -2,19 +2,20 @@ alert ("Bienvenidos a EmeLashes! Les recordamos que solo estamos tomando turnos 
 
 
 class hhTurnos  {
-    constructor (servicio, lashista, horario) {
+    constructor (servicio, lashista, horario, precio) {
     this.servicio = servicio;
     this.lashista= lashista;
     this.horario= horario;
+    this.precio= precio
     }
 }
 
 
-const turno1 = new hhTurnos ("extensiones", "Mariana", "9:00");
-const turno2 = new hhTurnos ("extensiones", "Mariana", "10:30");
-const turno3 = new hhTurnos ("extensiones", "Mariana", "12:00");
-const turno4 = new hhTurnos ("extensiones", "Mariana", "13:30");
-const turno5 = new hhTurnos ("extensiones", "Mariana", "15:00");
+const turno1 = new hhTurnos ("extensiones", "Mariana", "9:00", "1500");
+const turno2 = new hhTurnos ("extensiones", "Mariana", "10:30", "1500");
+const turno3 = new hhTurnos ("extensiones", "Mariana", "12:00", "1500");
+const turno4 = new hhTurnos ("extensiones", "Mariana", "13:30", "1500");
+const turno5 = new hhTurnos ("extensiones", "Mariana", "15:00", "1500");
 
 
 const turnos = [] 
@@ -24,6 +25,14 @@ turnos.push (turno3);
 turnos.push (turno4);
 turnos.push (turno5);
 
+
+const actualizado = turnos.map((pp) => {
+    return {
+        precio: pp.precio * 1.25
+    }
+})
+
+console.log(actualizado)
 
 
 let nombreCliente = prompt ("Ingrese su nombre");
